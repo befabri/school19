3e00a1b678be16078bfa2e1b6f9c28896e5c409c
befabri <me@D-Ue3k2.localdomain>
2021-09-07 21:50:01 +0200

    update

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -10,3 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_put_combn(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_put_combn(nb / 10);
+		ft_put_combn(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	if (n > 0 && n < 10)
+	{
+		while ()
+	}
+}
+
+int	main(void)
+{
+	ft_print_combn(2);
+	ft_putchar(',');
+	ft_putchar(' ');
+	return (0);
+}
