a0f987e9164c09ae3c6b637f9b8ccd6d444ab352
befabri <moi@DESKTOP-DSLUH9U.localdomain>
2021-09-12 14:03:59 +0200

    bfabri

--- c02/ex04/ft_str_is_lowercase.c
+++ c02/ex04/ft_str_is_lowercase.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/12 00:11:43 by bfabri            #+#    #+#             */
-/*   Updated: 2021/09/12 13:46:30 by bfabri           ###   ########.fr       */
+/*   Created: 2021/09/12 13:47:48 by bfabri            #+#    #+#             */
+/*   Updated: 2021/09/12 13:54:36 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_lowercase(char *str)
 {
 	int	c;
 
 	c = 0;
 	while (str[c] != '\0')
 	{
-		if (str[c] < 'A' || (str[c] > 'Z' && str[c] < 'a') || str[c] > 'z')
+		if (str[c] < 'a' || str[c] > 'z')
 		{
 			return (0);
 		}
--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/12 00:11:43 by bfabri            #+#    #+#             */
-/*   Updated: 2021/09/12 13:46:30 by bfabri           ###   ########.fr       */
+/*   Created: 2021/09/12 13:58:16 by bfabri            #+#    #+#             */
+/*   Updated: 2021/09/12 14:03:48 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_printable(char *str)
 {
 	int	c;
 
 	c = 0;
 	while (str[c] != '\0')
 	{
-		if (str[c] < 'A' || (str[c] > 'Z' && str[c] < 'a') || str[c] > 'z')
+		if (str[c] < '!' || str[c] > '~')
 		{
 			return (0);
 		}
