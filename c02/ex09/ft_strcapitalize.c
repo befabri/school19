e96bbcb0a3fc106e41f2a4a6c2f278256d6f47f2
befabri <moi@DESKTOP-DSLUH9U.localdomain>
2021-09-15 11:31:41 +0200

    bfabri

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 18:05:13 by bfabri            #+#    #+#             */
-/*   Updated: 2021/09/14 18:03:15 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/09/15 11:23:16 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,15 @@ int	ft_str_is_alpha(char c)
 	return (1);
 }
 
+int	ft_str_is_numeric(char c)
+{
+	if (c < '0' || c > '9')
+	{
+		return (0);
+	}
+	return (1);
+}
+
 char	*ft_strcapitalize(char *str)
 {
 	int	c;
@@ -41,13 +50,13 @@ char	*ft_strcapitalize(char *str)
 	str[0] = ft_strupcase(str[0]);
 	while (str[c] != '\0')
 	{
-		if (ft_str_is_alpha(str[c - 1]))
+		if (!ft_str_is_alpha(str[c - 1]) && !ft_str_is_numeric(str[c - 1]))
 		{
-			str[c] = ft_strlowcase(str[c]);
+			str[c] = ft_strupcase(str[c]);
 		}
 		else
 		{
-			str[c] = ft_strupcase(str[c]);
+			str[c] = ft_strlowcase(str[c]);
 		}
 		c++;
 	}
