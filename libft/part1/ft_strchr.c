569a7c2340cd65c179f6693abb88e3f8fe5dd063
Benjamin Fabri <bfabri@fu-r6-p3.s19.be>
2021-10-01 15:45:25 +0200

    Update

--- libft/part1/ft_strchr.c
+++ libft/part1/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:06:39 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/01 15:30:12 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/01 15:45:00 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ char *ft_strchr(const char *s, int c)
 			return ((char *) s + i);
 		i++;
 	}
-	if (s[i+1] == '\0' && c == '\0')
-		return ((char *) s + i + 1);
+	if (s[i] == '\0' && c == '\0') // TODO A verifier
+		return ((char *) s + i);
 	return (0);
 }
 
@@ -36,10 +36,14 @@ int	main(void)
 	char t3[] = "Ceci est UNE PhRase de test";
 	char t4[] = "Test yeah";
 	char t5[] = "";
+	char t6[] = "Test du backslash zero\0";
+	char t7[] = "\0Test du backslash zero";
 
 	printf("T1: %s (%s)\n", ft_strchr(t1, 'z'), strchr(t1, 'z'));
 	printf("T2: %s (%s)\n", ft_strchr(t2, '1'), strchr(t2, '1'));
 	printf("T3: %s (%s)\n", ft_strchr(t3, 'P'), strchr(t3, 'P'));
 	printf("T4: %s (%s)\n", ft_strchr(t4, ' '), strchr(t4, ' '));
 	printf("T5: %s (%s)\n", ft_strchr(t5, 'z'), strchr(t5, 'z'));
+	printf("T6: %s (%s)\n", ft_strchr(t6, '\0'), strchr(t6, '\0'));
+	printf("T7: %s (%s)\n", ft_strchr(t7, '\0'), strchr(t7, '\0'));
 }
--- libft/part1/ft_strrchr.c
+++ libft/part1/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:31:17 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/01 15:38:08 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/01 15:45:16 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ char *ft_strrchr(const char *s, int c)
 			return ((char *) s + size);
 		size--;
 	}
-	if (s[size - 1] == '\0' && c == '\0')
-		return ((char *) s + size - 1);
+	if (s[size] == '\0' && c == '\0') // TODO A verifier
+		return ((char *) s + size);
 	return (0);
 }
 
@@ -48,10 +48,14 @@ int	main(void)
 	char t3[] = "Ceci est UNE PhRase de test :D";
 	char t4[] = "Test yeah oh";
 	char t5[] = "";
+	char t6[] = "Test du backslash zero\0";
+	char t7[] = "\0Test du backslash zero";
 
 	printf("T1: %s (%s)\n", ft_strrchr(t1, 'Z'), strrchr(t1, 'Z'));
 	printf("T2: %s (%s)\n", ft_strrchr(t2, '8'), strrchr(t2, '8'));
 	printf("T3: %s (%s)\n", ft_strrchr(t3, 't'), strrchr(t3, 't'));
 	printf("T4: %s (%s)\n", ft_strrchr(t4, ' '), strrchr(t4, ' '));
 	printf("T5: %s (%s)\n", ft_strrchr(t5, 'z'), strrchr(t5, 'z'));
+	printf("T6: %s (%s)\n", ft_strrchr(t6, '\0'), strrchr(t6, '\0'));
+	printf("T7: %s (%s)\n", ft_strrchr(t7, '\0'), strrchr(t7, '\0'));
 }
