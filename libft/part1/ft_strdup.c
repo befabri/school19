9cca37d9f4a90a6be14600b58eb3b8dd5e258e68
Benjamin Fabri <bfabri@fu-r6-p3.s19.be>
2021-10-01 17:31:21 +0200

    Update

---
+++ libft/part1/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 17:19:12 by bfabri            #+#    #+#             */
+/*   Updated: 2021/10/01 17:19:15 by bfabri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		size;
+	int		i;
+
+	size = ft_strlen(src);
+	copy = (char *) malloc(sizeof(char) * (size + 1));
+	if (copy == NULL)
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
