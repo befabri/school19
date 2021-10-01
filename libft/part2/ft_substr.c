0fcdcbaf1ea34db6da3a5f8d6094cccf24f302d0
Benjamin Fabri <bfabri@fu-r6-p3.s19.be>
2021-10-01 16:33:43 +0200

    Update

---
+++ libft/part2/ft_substr.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/01 16:11:56 by bfabri            #+#    #+#             */
+/*   Updated: 2021/10/01 16:33:22 by bfabri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_substr(char const *s, unsigned int start, unsigned int len) // TODO verifier si \0 ce passe quoi dois ton break....
+{
+	char	*copy;
+	unsigned int		i;
+	int		size_str;
+
+	size_str = ft_strlen(s);
+	if (start < 0 || size_str < start)
+		return (0);
+	copy = (char *) malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (0);
+	i = 0;
+	*copy = 0;
+	while (len > 0)
+	{
+		copy[i] = s[start];
+		len--;
+		start++;
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	const char *test = "Phrase de test aled";
+
+	printf("-> %s \n", ft_substr(test, 15, 4));
+	return (0);
+}
