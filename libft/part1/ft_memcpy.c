8cf93a57ab45fc8c6e44e209e6b92b1e985259ac
Benjamin Fabri <bfabri@fu-r6-p3.s19.be>
2021-10-01 17:52:35 +0200

    Update

--- libft/part1/ft_memcpy.c
+++ libft/part1/ft_memcpy.c
@@ -6,16 +6,32 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 17:28:32 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/01 17:31:10 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/01 17:51:18 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*memcpy(void dst, const void src, size_t n)
+void	*ft_memcpy(void *dst, void *src, unsigned int n)
 {
+	int i;
 
+	(char *) src;
+	i = 0;
 
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+}
 
+#include <stdio.h>
+#include <string.h>
 
-	
-	return (dst);
+int main () {
+   const char src[50] = "phrase de test a afficher";
+   char dest[50] = "a remplacer";
+   printf("Avant dest = %s\n", dest);
+   memcpy(dest, src, 25+1);
+   printf("Apres dest = %s\n", dest);
+   
+   return(0);
 }
