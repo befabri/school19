6533c92599a3dc89bcb83a7626f8180593e99c7c
befabri <moi@DESKTOP-DSLUH9U.localdomain>
2021-10-03 20:26:32 +0200

    Update

--- libft/part1/ft_strlcpy.c
+++ libft/part1/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 14:28:22 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/03 19:51:24 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/03 20:26:21 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,26 @@ unsigned int	ft_strlcpy(char * restrict dst, const char * restrict src, unsigned
 }
 
 #include <stdio.h>
-#include <string.h>
+#include <bsd/string.h>
 
+// gcc ft_strlcpy.c -l bsd && ./a.out
 int	main(void)
 {
-	char	source[] = "moulinette";
-	char	destination[] = "phraseasupprimer";
-
+	char			source[] = "moulinette";
+	char			destination[] = "phraseasupprimer";
+	unsigned int	size;
+	unsigned int	size_destination;
+	unsigned int	i;
+	
+	size = 5;
 	printf("Source -> %s\n", source);
 	printf("Destination -> %s\n", destination);
-	ft_strcpy(destination, source);
+	unsigned int a = strlcpy(destination, source, size);
 	printf("----- AFTER ft_strcpy -----\n");
+	printf("Size src return: %d\n", a);
 	printf("Destination -> %s\n", destination);
-	int size_destination = sizeof(destination);
-	int i = 0;
+	size_destination = sizeof(destination);
+	i = 0;
 	while(i<size_destination)
 	{
 		if(destination[i] == '\0') {
@@ -55,5 +61,6 @@ int	main(void)
 		printf("%c",destination[i]);
 		i++;
 	}
+	printf("\n");
 	return (0);
 }
