b3002490d08b4fe113bc5c353591067ccd110b70
Benjamin Fabri <bfabri@fu-r8-p2.s19.be>
2021-10-08 15:16:01 +0200

    update

--- libft/part1/ft_bzero.c
+++ libft/part1/ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 13:49:33 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/03 20:51:27 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/08 14:31:27 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(void *s, int n)
+void	ft_bzero(void *s, unsigned int n)
 {
-	int		c;
+	unsigned int	c;
 	char	*s_tmp;
 
 	s_tmp = (char *) s;
@@ -44,25 +44,27 @@ void	ft_bzero(void *s, int n)
 // 	int		i;
 // 	int		size;
 
-// 	// ft_bzero(t1, 10);
-// 	// ft_bzero(t2, 5);
-// 	// ft_bzero(t3, 7);
-// 	// ft_bzero(t4, 1);
-// 	// ft_bzero(t5, 0);
+// 	ft_bzero(t1, 10);
+// 	ft_bzero(t2, 5);
+// 	ft_bzero(t3, 7);
+// 	ft_bzero(t4, 1);
+// 	ft_bzero(t5, 0);
 // 	bzero(t1bis, 10);
 // 	bzero(t2bis, 5);
 // 	bzero(t3bis, 7);
 // 	bzero(t4bis, 1);
 // 	bzero(t5bis, 0);
 
-// 	printf("t1: %s \nAttendu: %s \n", t1, t1bis);
+// 	printf("t1: %s \n -> %s \n", t1, t1bis);
 // 	i = 0;
 // 	size = 50;
 // 	while (i < size)
 // 	{
 // 		if (t1bis[i] == '\0')
 // 			t1bis[i] = '|';
+// 		if (t1[i] == '\0')
+// 			t1[i] = '|';
 // 		i++;
 // 	}
-// 	printf("t1: %s \nAttendu: %s \n", t1, t1bis);
+// 	printf("t1: %s \n -> %s \n", t1, t1bis);
 // }
---
+++ libft/part2/ft_split.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 14:36:54 by bfabri            #+#    #+#             */
+/*   Updated: 2021/10/08 15:15:54 by bfabri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_countchar(const char *s, char c)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+/*
+Alloue (avec malloc(3)) et retourne un tableau
+de chaines de caracteres obtenu en séparant ’s’ à
+l’aide du caractère ’c’, utilisé comme délimiteur.
+Le tableau doit être terminé par NULL.
+*/
+char **ft_split(char const *s, char c)
+{
+	char	*copy;
+	int		i;
+	int		nb;
+	int		j;
+	char	*copy_tmp;
+	if (!c)
+	{
+		copy = malloc(sizeof(char));
+		*copy = 0;
+		return (copy);
+	}
+	nb = ft_countchar(s, c);
+	copy = (char *) malloc(sizeof(char) * nb);
+	if (copy == NULL)
+		return (0);
+	i = 0;
+	*copy = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			j = i;
+			copy_tmp = (char *) malloc(sizeof(char) * (j + 1));
+			
+			if (copy_tmp == NULL)
+				return (0);
+			
+			nb++;
+			j = 0;
+		}
+		i++;
+	}
+	return (copy);
+}
