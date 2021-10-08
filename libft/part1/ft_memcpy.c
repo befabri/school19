8d77807f3d1a832c2acf7bbaea45fe0d611b84c4
befabri <moi@DESKTOP-DSLUH9U.localdomain>
2021-10-08 11:54:40 +0200

    update

--- libft/part1/ft_memcpy.c
+++ libft/part1/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bfabri <bfabri@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 17:28:32 by bfabri            #+#    #+#             */
-/*   Updated: 2021/10/03 20:47:30 by bfabri           ###   ########.fr       */
+/*   Updated: 2021/10/07 13:11:56 by bfabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,31 @@ void	*ft_memcpy(void *dst, const void *src, unsigned int n)
 	return (dst);
 }
 
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int	main (void)
-// {
-// 	const char	src_1[50] = "phrase de test a afficher";
-// 	char		dst_1[] = "a remplacer";
-// 	const char	src_2[50] = "phrase de test a afficher";
-// 	char		dst_2[] = "a remplacer";
+int	main (void)
+{
+	const char	src_1[50] = "phrase de test a afficher";
+	char		dst_1[] = "a remplacer";
+	const char	src_2[50] = "phrase de test a afficher";
+	char		dst_2[] = "a remplacer";
 
-// 	printf("TEST 1\n");
-// 	printf("Src: %s\n", src_1);
-// 	printf("Dst: %s\n", dst_1);
+	printf("TEST 1\n");
+	printf("Src: %s\n", src_1);
+	printf("Dst: %s\n", dst_1);
 
-// 	ft_memcpy(0, 0, strlen(src_1) + 1);
-// 	//ft_memcpy(dst_1, 0, strlen(src_1) + 1);
-// 	ft_memcpy(dst_1, src_1, strlen(src_1) + 1);
-// 	printf("ft_memcpy Src: %s\n", src_1);
-// 	printf("ft_memcpy Dst: %s\n", dst_1);
+	//ft_memcpy(0, 0, strlen(src_1) + 1);
+	//ft_memcpy(dst_1, 0, strlen(src_1) + 1); // Segfault
+	ft_memcpy(dst_1, src_1, strlen(src_1) + 1);
+	printf("ft_memcpy Src: %s\n", src_1);
+	printf("ft_memcpy Dst: %s\n", dst_1);
 
-// 	memcpy(0, 0, strlen(src_2) + 1);
-// 	//memcpy(dst_2, 0, strlen(src_2) + 1);
-// 	memcpy(dst_2, src_2, strlen(src_2) + 1);
-// 	printf("memcpy Src: %s\n", src_2);
-// 	printf("memcpy Dst: %s\n", dst_2);
+	//memcpy(0, 0, strlen(src_2) + 1);
+	//memcpy(dst_2, 0, strlen(src_2) + 1); // Segfault
+	memcpy(dst_2, src_2, strlen(src_2) + 1);
+	printf("memcpy Src: %s\n", src_2);
+	printf("memcpy Dst: %s\n", dst_2);
 
-// 	return(0);
-// }
+	return(0);
+}
